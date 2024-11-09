22127b20952f56dd2ada0aed670589ed8bea41d2
JDSB05 <94171510+JDSB05@users.noreply.github.com>
2024-11-09 11:03:57 +0000

    remover: eliminar log de token no interceptor de requisições

--- frontend/config/api.ts
+++ frontend/config/api.ts
@@ -18,7 +18,6 @@ api.interceptors.request.use(
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
-    console.log('Token:', token); 
     return config;
   },
   (error) => {
